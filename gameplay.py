73dbb1f8a1169eb884b4504f0e241e34d09a4346
pg024 <50447024+pg024@users.noreply.github.com>
2019-05-09 19:55:17 +0200

    Update gameplay.py

--- gameplay.py
+++ gameplay.py
@@ -15,7 +15,6 @@ def bivio():
 	elif "sx" in scelta.lower():
 	 sacchetto = random.randint(1, 50)
 	 print(f"La strada è libera, per terra trovi un sacchetto con {sacchetto} scellini belgi")
-	 tesoro = sacchetto
 	 nazi()
 	else: 
 	 print("non ho capito la tua scelta, riprova")
@@ -26,48 +25,49 @@ def last_step():
 	exit(0)
 
 def morra():
-	score_hero = 0
-	score_nazi = 0
-	print("Al meglio delle tre, GO!\n")
-	
-	while score_hero < 3 and score_nazi < 3:
-	 print("Il tuo score è: ", score_hero)
- 	 print("Lo score del nazi è: ", score_nazi)
-	 print("Scegli: [1]Carta [2]Forbice [3]Sasso")
-	 lista = ['carta', 'forbice', 'sasso']
- 	 colpo = lista[int(input("> "))-1]
- 	 colponazi = random.choice(lista)
-	 print(f"il tuo avversario ha scelto {colponazi}\n")
- 	 if colpo == colponazi:
-	  print("pareggio, come on!\n")
-	 elif (colpo == 'carta' and colponazi == 'forbice') or (colpo == 'forbice' and colponazi == 'sasso') or (colpo == 'sasso' and colponazi == 'carta'):
- 	  print("argh!\n")
-	  score_nazi = score_nazi + 1
- 	 else:
- 	  print("un punto per noi!\n")
- 	  score_hero = score_hero + 1
+ score_hero = 0
+ score_nazi = 0
+ print("Al meglio delle tre, GO!\n")
+		
+ while score_hero < 3 and score_nazi < 3:
+  print("Il tuo score è: ", score_hero)
+  print("Lo score del nazi è: ", score_nazi)
+  print("Scegli: [1]Carta [2]Forbice [3]Sasso")
+  lista = ['carta', 'forbice', 'sasso']
+  colpo = lista[int(input("> "))-1]
+  colponazi = random.choice(lista)
+  print(f"il tuo avversario ha scelto {colponazi}\n")
+ 		
+  if colpo == colponazi:
+   print("pareggio, come on!\n")
+  elif (colpo == 'carta' and colponazi == 'forbice') or (colpo == 'forbice' and colponazi == 'sasso') or (colpo == 'sasso' and colponazi == 'carta'):
+   print("argh!\n")
+   score_nazi = score_nazi + 1
+  else:
+   print("un punto per noi!\n")
+   score_hero = score_hero + 1
 
-	if score_nazi == 3:
- 	 print("Hai perso! Il nazi ti picchia")
-	 return dead()
-	else:
- 	 print("Olè, VITTORIA!")
-	 return last_step()
+  if score_nazi == 3:
+   print("Hai perso! Il nazi ti picchia")
+   return dead()
+  else:
+   print("Olè, VITTORIA!")
+   return last_step()
 	 	 
 	
-def nazi()
-	print("""Un bullo ti sbarra la strada,
-		 Vuoi provare a corromperlo con il tuo tesoro?""")
-	scelta = input("Yes/No ")
-	if "yes" in scelta.lower():
-	 minimo = random.randint(1, 50)
-	 if minimo >= tesoro:
-	  dead()
-	 else:
-	  print("Il bullo accetta i tuoi soldi e ti lascia passare, ben fatto!")
-	  prosegui()
-        if "no" in scelta.lower():
-	 print("Ok allora lo sfidi a morra cinese")
-	 morra()
+def nazi():
+ print("""Un bullo ti sbarra la strada,
+          Vuoi provare a corromperlo con il tuo tesoro?""")
+ scelta = input("Yes/No ")
+ if "yes" in scelta.lower():
+  minimo = random.randint(1, 50)
+  if minimo >= sacchetto:
+   dead()
+  else:
+   print("Il bullo accetta i tuoi soldi e ti lascia passare, ben fatto!")
+ last_step()
+ if "no" in scelta.lower():
+  print("Ok allora lo sfidi a morra cinese")
+  morra()
 	 
 bivio()
